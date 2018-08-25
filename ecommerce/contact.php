8009b12079d3deb08adcc17e1e868544161a9292
RashikIslam007 <rashik.islam12@gmail.com>
2018-08-25 16:39:49 +0600

    ecom

---
+++ ecommerce/contact.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+</head>
+<body bgcolor="pink">
+<form action="" method="post">
+
+
+<h2 align="center">For any information please email us at :</h2><br>
+<h2 align="center">najoa@yahoo.com</h2><br>
+<h2 align="center">naznin@yahoo.com</h2><br>
+
+<a href="index.php"style="color:blue">Go Back</a>
+
+
+</form>
+</body>
+</html>
